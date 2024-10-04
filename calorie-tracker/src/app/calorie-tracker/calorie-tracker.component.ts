c4c6cddb83c39e3d3dd4ecdc180d209387102167
tylerhuynh <tyhu7637@colorado.edu>
2024-10-04 14:05:35 -0600

    Init commit

---
+++ calorie-tracker/src/app/calorie-tracker/calorie-tracker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calorie-tracker',
+  templateUrl: './calorie-tracker.component.html',
+  styleUrl: './calorie-tracker.component.scss'
+})
+export class CalorieTrackerComponent {
+  
+
+}
